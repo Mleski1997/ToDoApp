c1765622857db195bcc07f5665b3cb976bdacf89
Michał Łęski <Mleski1997@gmail.com>
2023-04-12 14:18:52 +0200

    completed and sort

--- ToDoApp.cs
+++ ToDoApp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,7 @@ using System.Xml.Serialization;
 
 namespace ToDoList
 {
-    internal class ToDoApp
+    public class ToDoApp
     {
         List<string> toDoListApp = new List<string>();
         public void IntroduceToDo()
@@ -32,15 +33,35 @@ namespace ToDoList
             toDoListApp.RemoveAt(taskNumber);
         }
 
+        public void selectTaskToCompleted()
+        {
+            showList();
+           
+
+            int taskNumber = Convert.ToInt32(Console.ReadLine());
+            string completedTask = toDoListApp[taskNumber];
+            string cmpt = "Completed" + " : " + completedTask;
+
+            Console.WriteLine(cmpt);
+            toDoListApp[taskNumber] = cmpt;
+          
+           
+
+
+
+
+
+        }
+
         public void editTodo()
         {
-            Console.WriteLine("Select task number to edir");
+            Console.WriteLine("Select task number to edit");
             showList();
             int taskNumber = Convert.ToInt32(Console.ReadLine());
            
             Console.WriteLine(toDoListApp[taskNumber]);
             String taskToEdit = toDoListApp[taskNumber];
-            Console.WriteLine("What word you do want replace?");
+            Console.WriteLine("What word do you want replace?");
             string oldWord = Console.ReadLine();
             Console.WriteLine("Please write a new word");
             string newWord = Console.ReadLine();
@@ -59,22 +80,28 @@ namespace ToDoList
             for (int i = 0; i < toDoListApp.Count; i++)
             {
                 Console.WriteLine(i + " : " + toDoListApp[i]);
+                toDoListApp.Sort();
 
             }
+
+          
+
+
         }
 
         public void askWhatDo()
         {
 
             string option = " ";
-            while (option != "5")
+            while (option != "6")
             {
                 Console.WriteLine("What do you want do");
                 Console.WriteLine("1. Add task");
                 Console.WriteLine("2. Remove task");
                 Console.WriteLine("3. Edit task");
                 Console.WriteLine("4. Show task");
-                Console.WriteLine("5. Exit");
+                Console.WriteLine("5. Select a task to complete");
+                Console.WriteLine("6. Exit");
 
 
                 option = Console.ReadLine();
@@ -93,8 +120,14 @@ namespace ToDoList
                 {
                     showList();
 
+                } else if (option == "5")
+                    
+                {
+                    selectTaskToCompleted();
+
+
                 }
-                else if (option == "5")
+                else if (option == "6")
                 {
                     Console.WriteLine("Exiting progrsam ");
 
